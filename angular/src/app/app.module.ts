1c708b008092bb1cf7dd2b5cf29c148fd62c7343
CCatlin28 <christy.catlin@revature.net>
2021-09-14 22:06:55 -0500

    css and perimiter components

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
+import { SocialComponent } from './components/social/social.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    MainNavComponent,
+    SocialComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
