25420810cea108020a6124a7bee3b4ed8326b610
mohamadelshalati <mohamad.elshalati@revature.net>
2021-09-19 14:29:38 -0400

    cart

---
+++ angular/Cart/cart.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  public cartItems: any = [];
+  public books = new BehaviorSubject<any>([]);
+
+  constructor() { }
+
+  getBooks(){
+    return this.books.asObservable();
+  }
+
+
+  addtoCart(book : any){
+    this.cartItems.push(book);
+    this.books.next(this.cartItems);
+    console.log(this.cartItems);
+    console.log(this.books);
+  }
+}
---
+++ angular/Cart/navbar/navbar.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CartService } from 'src/app/services/cart.service';
+import { UserService } from 'src/app/services/user.service';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+
+  public user:any;
+  public cartItems: number = 0;
+
+  constructor(private router:Router, private cartService:CartService, private userService:UserService) { }
+
+  ngOnInit(): void {
+    this.cartService.getBooks().subscribe(data =>{
+      this.cartItems = data.length;
+    })
+
+    this.userService.getUser().subscribe(data =>{
+      this.user = data;
+    })
+  }
+
+ getNew(){
+  this.router.navigate(['/new'])
+ }
+
+ login(){
+   this.router.navigate(['/login'])
+ }
+
+ register(){
+  this.router.navigate(['/register'])
+}
+
+cart(){
+  this.router.navigate(['/cart'])
+}
+
+}
